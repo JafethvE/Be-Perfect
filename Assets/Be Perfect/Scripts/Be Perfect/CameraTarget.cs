4123af1afebc4fb36f079faa320b027b782e150e
Jafeth <Jafeth.vanElten@hotmail.com>
2019-07-28 21:09:42 +0200

    Implement Camera Rotate in Sample Scene.

---
+++ Assets/Be Perfect/Scripts/Be Perfect/CameraTarget.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour
+{
+    [SerializeField]
+    private new Transform transform;
+
+    [SerializeField]
+    private float rotationSpeed;
+
+    private void Update()
+    {
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+    }
+}
